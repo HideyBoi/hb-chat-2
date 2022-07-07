c9bb9d94cf718985fd1b995b8d339938dedc8c26
Hidey Boi <maxg2070319@gmail.com>
2022-07-06 23:22:39 -0500

    testing
    lol nerd

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker by passing the generated config
+if (!firebase.apps.length) {
+	firebase.initializeApp({
+    	apiKey: "AIzaSyBpWmtGTqluiA1gC0CEFHrQcZncNRYtjbg",
+    	authDomain: "private-school-chat.firebaseapp.com",
+		databaseURL: "https://private-school-chat.firebaseio.com",
+    	projectId: "private-school-chat",
+    	storageBucket: "private-school-chat.appspot.com",
+    	messagingSenderId: "113930891891",
+    	appId: "1:113930891891:web:88a3cb1a24e25b06017006"
+	});
+} else {
+	firebase.app();
+}
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
